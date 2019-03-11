1152246f1d04877a598a1eef6f0e5d72c940cb54
Jainrohan343 <37058422+Jainrohan343@users.noreply.github.com>
2019-03-11 16:03:35 +0530

    other

--- test1.java
+++ test1.java
@@ -2,7 +2,7 @@ public Class Hello{
 public static void main(String[] args)
 {
 for (int i=1;i<10;i++)
-{System.out.println("Hello World....."+i);
+{System.out.println("Hello other World....."+i);
  
  
 }
